d9d157c012939b52eabcc9502aa1e5f17ad820a4
Irismar <128446136+Irismar04@users.noreply.github.com>
2023-06-18 14:25:02 -0400

    cambios

--- app/Views/anadir.view.php
+++ app/Views/anadir.view.php
@@ -65,12 +65,12 @@
                             <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
                                 <nav class="sb-sidenav-menu-nested nav accordion" id="sidenavAccordionPages">
                                     <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseAuth" aria-expanded="false" aria-controls="pagesCollapseAuth">
-                                        Agregar Categorias
+                                        Agregar categorias
                                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                                     </a>
                                     
                                     <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#pagesCollapseError" aria-expanded="false" aria-controls="pagesCollapseError">
-                                        Eliminar Categorias
+                                        Eliminar categorias
                                         <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
                                     </a>
                             </div>
